29bee9793017449aad6b5a22367eede739f681be
Hinrek Saar <hinrek@gmail.com>
2018-12-06 21:10:27 +0200

    Using RandomForestClassifier result 95.9

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as numpy
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier # 83.....
+from sklearn.ensemble import RandomForestClassifier # 92.7...
 
 data = pd.read_csv("dataset/train.csv").as_matrix()
 print(data)
-classifier = DecisionTreeClassifier()
+
+# Default n_estunators=10
+# Result 92.7..
+# Current result 95.9..
+classifier = RandomForestClassifier(n_estimators=100)
 
 #Training dataset
 xtrain = data[0:21000, 1:]
@@ -17,7 +22,6 @@ xtest = data[21000:, 1:]
 actual_label = data[21000:, 0]
 
 prediction = classifier.predict(xtest)
-
 
 count = 0
 for i in range(0, 21000):
